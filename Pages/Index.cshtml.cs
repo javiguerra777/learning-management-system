7a592783ed735d646eff8b8d162be4e1f0483a1f
beary <javier.guerra1001@gmail.com>
2024-05-20 11:04:16 -0700

    feat(Registration UI): completed building out the UI for the login and registration pages, barring one image for the registration page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,7 +6,10 @@ namespace learning_management_system.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-
+    [BindProperty]
+    public string? Email { get; set; }
+    [BindProperty]
+    public string? Password { get; set; }
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
@@ -16,5 +19,9 @@ public class IndexModel : PageModel
     {
 
     }
-    public void OnPost(string email, string password) {}
+    public IActionResult OnPost() {
+        Console.WriteLine("Email: " + Email);
+        Console.WriteLine("Password: " + Password);
+        return Page();
+    }
 }
