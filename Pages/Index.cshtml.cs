da690f513bb2e556f01877fabe6f25abffb966b0
Javier Guerra <javier.guerra1001@gmail.com>
2024-06-24 22:45:09 -0700

    feat(registration): user is able to register an account, and they are able to log in, email and password registration works correctly and passwords are hashed and saved in the database securely

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,27 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using learning_management_system.Models;
+using learning_management_system.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace learning_management_system.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly PasswordHasher<IndexModel> _passwordHasher = new PasswordHasher<IndexModel>();
+
     [BindProperty]
     public string? Email { get; set; }
     [BindProperty]
     public string? Password { get; set; }
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public void OnGet()
     {
 
     }
-    public IActionResult OnPost() {
-        Console.WriteLine("Email: " + Email);
-        Console.WriteLine("Password: " + Password);
-        return Page();
+    public async Task<IActionResult> OnPostAsync() {
+        if(!ModelState.IsValid) {
+            return Page();
+        }
+        var validEmail = Email.Trim().ToLower();
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == validEmail);
+        if (user == null) {
+            ModelState.AddModelError("Email", "Email does not exist");
+            return Page();
+        }
+        var comparePassword = _passwordHasher.VerifyHashedPassword(this, user.PasswordHash, Password);
+        if (comparePassword == PasswordVerificationResult.Failed) {
+            ModelState.AddModelError("Password", "Incorrect Password");
+            return Page();
+        }
+        _logger.LogInformation("Successful Login!");
+        return RedirectToPage("/home");
     }
 }
--- Pages/Registration.cshtml.cs
+++ Pages/Registration.cshtml.cs
@@ -1,19 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using learning_management_system.Data;
+using learning_management_system.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace learning_management_system.Pages;
 
 public class RegistrationModel : PageModel
 {
     private readonly ILogger<RegistrationModel> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly PasswordHasher <RegistrationModel> _passwordHasher = new PasswordHasher<RegistrationModel>();
+    
+    [BindProperty]
+    public string? Email { get; set; }
+    [BindProperty]
+    public string? Password { get; set; }
+    [BindProperty]
+    public string? FullName { get; set; }
 
-    public RegistrationModel(ILogger<RegistrationModel> logger)
+    public RegistrationModel(ILogger<RegistrationModel> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public void OnGet()
     {
 
+    }
+    public async Task<IActionResult> OnPostAsync() {
+        if(!ModelState.IsValid) {
+            return Page();
+        }
+        var validEmail = Email.Trim().ToLower();
+        var trimmedPassword = Password.Trim();
+        var trimmedFullName = FullName.Trim();
+        var hashedPassword = _passwordHasher.HashPassword(this, trimmedPassword);
+        bool emailExists = await _context.Users.AnyAsync(u => u.Email == validEmail);
+        if (emailExists) {
+            ModelState.AddModelError("Email", "Email already exists");
+            return Page();
+        }
+        var user = new UserModel {
+            Email = validEmail,
+            PasswordHash = hashedPassword,
+            FullName = trimmedFullName,
+            Points = 0
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("Successful Registration!");
+        return RedirectToPage("/home");
     }
 }
